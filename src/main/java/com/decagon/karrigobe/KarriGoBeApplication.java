c9f87c9288b6d6575898d81ae99c6bafcd7406ae
samjayworldwide <samuel.mbanisi@decagon.dev>
2023-11-08 15:24:22 +0100

    karrigo2023

---
+++ src/main/java/com/decagon/karrigobe/KarriGoBeApplication.java
@@ -0,0 +1,23 @@
+package com.decagon.karrigobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarriGoBeApplication {
+    /*
+    echo "# karrigo-2023" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/Samjayworldwide/karrigo-2023.git
+    git push -u origin main
+
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarriGoBeApplication.class, args);
+    }
+
+}
